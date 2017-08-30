1bb744a0abba839629d3b6ff30607861d667cbee
Eduardo Rivas <e@jerivas.com>
2017-08-29 22:26:47 -0600

    Comment out theme models and admin.

--- project_template/theme/admin.py
+++ project_template/theme/admin.py
@@ -1,25 +1,25 @@
-from __future__ import unicode_literals
-from copy import deepcopy
+# from __future__ import unicode_literals
+# from copy import deepcopy
 
-from django.contrib import admin
+# from django.contrib import admin
 
-from mezzanine.core.admin import StackedDynamicInlineAdmin, TabularDynamicInlineAdmin
-from mezzanine.pages.admin import PageAdmin
-from mezzanine.pages.models import RichTextPage
-from mezzanine.utils.admin import SingletonAdmin
+# from mezzanine.core.admin import StackedDynamicInlineAdmin, TabularDynamicInlineAdmin
+# from mezzanine.pages.admin import PageAdmin
+# from mezzanine.pages.models import RichTextPage
+# from mezzanine.utils.admin import SingletonAdmin
 
-from .models import SiteConfiguration
+# from .models import SiteConfiguration
 
-# Page (extra model fields)
-# page_fieldsets = deepcopy(PageAdmin.fieldsets)
-# page_fieldsets[0][1]["fields"].insert(1, "field1")
-# PageAdmin.fieldsets = page_fieldsets
+# # Page (extra model fields)
+# # page_fieldsets = deepcopy(PageAdmin.fieldsets)
+# # page_fieldsets[0][1]["fields"].insert(1, "field1")
+# # PageAdmin.fieldsets = page_fieldsets
 
-# admin.site.unregister(RichTextPage)
-# admin.site.register(RichTextPage, PageAdmin)
+# # admin.site.unregister(RichTextPage)
+# # admin.site.register(RichTextPage, PageAdmin)
 
 
-# Site Configuration
-@admin.register(SiteConfiguration)
-class SiteConfigurationAdmin(SingletonAdmin):
-    pass
+# # Site Configuration
+# @admin.register(SiteConfiguration)
+# class SiteConfigurationAdmin(SingletonAdmin):
+#     pass
--- project_template/theme/models.py
+++ project_template/theme/models.py
@@ -1,26 +1,26 @@
-from __future__ import unicode_literals
+# from __future__ import unicode_literals
 
-from django.db import models
-from django.utils.encoding import python_2_unicode_compatible
+# from django.db import models
+# from django.utils.encoding import python_2_unicode_compatible
 
-from mezzanine.core.fields import FileField
-from mezzanine.core.models import SiteRelated
+# from mezzanine.core.fields import FileField
+# from mezzanine.core.models import SiteRelated
 
 
-####################
-# Site configuration
-####################
+# ####################
+# # Site configuration
+# ####################
 
-@python_2_unicode_compatible
-class SiteConfiguration(SiteRelated):
-    """
-    Singleton model for storing site-wide variables.
-    """
-    logo = FileField("Logo", upload_to="site/logo", format="Image", blank=True)
-    footer_content = models.TextField("Footer content", max_length=1000)
+# @python_2_unicode_compatible
+# class SiteConfiguration(SiteRelated):
+#     """
+#     Singleton model for storing site-wide variables.
+#     """
+#     logo = FileField("Logo", upload_to="site/logo", format="Image", blank=True)
+#     footer_content = models.TextField("Footer content", max_length=1000)
 
-    def __str__(self):
-        return "Site Configuration"
+#     def __str__(self):
+#         return "Site Configuration"
 
-    class Meta:
-        verbose_name = verbose_name_plural = "Site Configuration"
+#     class Meta:
+#         verbose_name = verbose_name_plural = "Site Configuration"
