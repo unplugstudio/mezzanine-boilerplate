902309ae0abe69dd0b2b2af4c0f912bb117c85d2
Eduardo Rivas <e@jerivas.com>
2017-08-31 09:24:26 -0600

    Toggle classes to indicate if JS is enabled.

--- project_template/theme/static/js/main.js
+++ project_template/theme/static/js/main.js
@@ -1,3 +1,8 @@
 // Main site scripts
 
 import '../scss/main.scss';
+
+// Replace the 'no-js' class with the 'js' class on <html>
+// Useful when CSS needs to determine if JS is enabled
+document.documentElement.classList.remove('no-js');
+document.documentElement.classList.add('js');
